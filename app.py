08bcb140a037ece3ebb9fc36ed864a86abc63f15
Anna Liner <annaliner89@gmail.com>
2023-11-17 12:22:51 -0600

    added required to form input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import request as stock_request
 import webbrowser 
 import threading 
@@ -6,6 +6,8 @@ import os
 
 api_key = 'UKYXF61L981EG9X3'
 app = Flask(__name__)
+# no secret key?
+# app.config['SECRET_KEY'] = 'your secret key'
 
 def open_browser():
     print("Opening browser")
@@ -24,21 +26,12 @@ def get_stock_data():
     end_date = request.form['end_date']
 
     time_series = int(time_series_str)
-    # print(time_series)
-# time is just for intraday(find a way to work it in)
+    
+    # time is just for intraday(find a way to work it in)
     data = stock_request.retrieve_data(time_series, stock_symbol, api_key, time=None)
-# data = retrieve_data(time_series, stock_symbol, api_key, time)
     chart_html = stock_request.generate_line_chart_html(data, title=f"Stock Data for {stock_symbol}: {start_date} to {end_date}", bar_chart_type=chart_type, time_series=time_series, start_date=None, end_date=None, time=None)
-# generate_line_chart_html(data, title='Stock Price Chart',bar_chart_type=1, time_series=1, start_date=None, end_date=None, time=None):
-    
-    
     
     return chart_html
-
-
-
-
-
 
 if __name__ == '__main__':
     threading.Timer(1.25, open_browser).start()
