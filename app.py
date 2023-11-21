08550b60f643c9063449df7c1de7f3166d6efbdb
Anna Liner <annaliner89@gmail.com>
2023-11-21 15:18:13 -0600

    added error handling

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import webbrowser
 import threading 
 import os
 
-
 api_key = 'UKYXF61L981EG9X3'
 app = Flask(__name__)
 
@@ -18,31 +17,32 @@ def home():
 
 @app.route('/get_stock_data', methods=['POST'])
 def get_stock_data():
-    stock_symbol = request.form['stock_symbol']
-    time_series_str = request.form['time_series']
-    chart_type = request.form['chart_type']
-    start_date = request.form['start_date']
-    end_date = request.form['end_date']
 
-    time_series = int(time_series_str)
-    # Hardcoded 15min for intraday so it actually displays
-    if time_series_str == '1':
-        data = stock_request.retrieve_data(time_series, stock_symbol, api_key, time='15min')
-        chart_html = stock_request.generate_line_chart_html(data, title=f"Stock Data for {stock_symbol}: {start_date} to {end_date}", bar_chart_type=chart_type, time_series=time_series, start_date=None, end_date=None, time='15min')
-    else: 
+    try:
+
+        stock_symbol = request.form['stock_symbol']
+        time_series_str = request.form['time_series']
+        chart_type = request.form['chart_type']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+
+        time_series = int(time_series_str)
         data = stock_request.retrieve_data(time_series, stock_symbol, api_key, time=None)
         chart_html = stock_request.generate_line_chart_html(data, title=f"Stock Data for {stock_symbol}: {start_date} to {end_date}", bar_chart_type=chart_type, time_series=time_series, start_date=None, end_date=None, time=None)
+        
+        return chart_html
 
-    return chart_html
-
-
-
-
-
+    # bad request
+    except KeyError as e:
+        error = f"The data entered is missing or invalid: {e}"
+        return error, 400
+    
+    # server error 
+    except Exception as e:
+        error = f"Error occurred: {e}"
+        return error, 500 
 
 if __name__ == '__main__':
-    # Added this to prevent a second window from opening up
-    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-        threading.Timer(1.25, open_browser).start()
+    threading.Timer(1.25, open_browser).start()
     app.run(debug=True)
     
